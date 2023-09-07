19f9f69c54eb1f2f26c873ddd1cf5ea408db22e9
YuliiaShchedrakova <wedrak@ukr.net>
2023-09-07 10:41:38 +0200

    07/09/2023 Exercices Operateurs

---
+++ ex_operateurs1.js
@@ -0,0 +1,20 @@
+var a="100"; 
+var b=100; 
+var c=1.00;
+var d=true;
+
+// Exercice 1
+window.alert("Ceci est une chaine de caracteres: " +a);
+
+// Exercice 2
+b--; 
+f=b;
+window.alert("Le résultat de l'application de l'operateur de décrémentation à une variable b : " +b); 
+
+// Exercice 3 
+e=c+a;
+window.alert("Le résultat de l'agut de la valeur c à la valeur a : " + e);  
+
+// Exercice 4
+j = !d; 
+window.alert("Le résultat de l'inversation de la valeur de la variable d : " + j);
