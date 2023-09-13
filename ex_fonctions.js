bc828006865adfbfe1bb2ff95c792d5b0cc3f766
YuliiaShchedrakova <wedrak@ukr.net>
2023-09-13 14:52:03 +0200

    corrigé

--- ex_fonctions.js
+++ ex_fonctions.js
@@ -1,42 +1,56 @@
 // // Exercice 1
 
-var x=window.prompt("Ecrire le nombre 1 : "); 
-var y=window.prompt("Ecrire le nombre 2 : "); 
-console.log(x);
-console.log(y);
+// var x=window.prompt("Ecrire le nombre 1 : "); 
+// var y=window.prompt("Ecrire le nombre 2 : "); 
+// console.log(x);
+// console.log(y);
 
 
-y.innerHTML;  
+// var imgsrc="img/papillon.jpg";
 
-    var c=x*x*x; 
-    console.log(c);
+// y.innerHTML;  
 
-    var p=x*y; 
-    console.log(p);
+//     var c=x*x*x; 
+//     console.log(c);
+
+//     var p=x*y; 
+//     console.log(p);
+
+
+
+
+// // var imagejavascript="img/papillon.jpg";
+
+// document.getElementById("myP").innerHTML = "Le cube de " +x+ " est égal à " + c;
+// document.getElementById("myP2").innerHTML ="Le produit de " + x+ " et " +y +" est égal à " + p;
+
+
+
+
+// function afficheImg(imgsrc)
+// {
+//     var imagejavascript=document.getElementById("myI"); 
+//     imagejavascript.setAttribute("src",imgsrc);
+// }
+// afficheImg(imgsrc);
+
+// console.log("Fin de la boucle");
+
+// // Exercice 2
+
+var str1="yuliia; schedrakova; ukraininne; perdue; en; piccardie"; 
+console.log(typeof str1);
+var str2=";";  
+
+resultat=str1.split(str2, [6]);  
+// window.alert(resultat);
+// r=strtok(resultat, [2]);
+
+// window.alert(r);
 
 console.log("Fin de la boucle");
 
 
 
-document.getElementById("myP").innerHTML = "Le cube de " +x+ " est égal à " + c;
-document.getElementById("myP2").innerHTML ="Le produit de " + x+ " et " +y +" est égal à " + p;
-
-var imagejavascript="img/papillon.jpg";
-
-function afficheImg(imagejavascript)
-{
-imagejavascript.setAttribute("src", "img/papillon.jpg");
-}
-afficheImg("img");
 
 
-
-// // Exercice 2
-
-// var str1=window.prompt("Ecrire une liste de mots séparés par le caractère ; :");
-// var str2=
-// strtok(str1, ";", 2); 
-// var str1=str.split (''); 
-// console.log(str1 [2]);
-
-// window.alert(strtok);
