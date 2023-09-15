4788cd7b72f3b0bc1bd71ab3e3be11771fb7814d
YuliiaShchedrakova <wedrak@ukr.net>
2023-09-15 12:12:32 +0200

    15/09/2023

--- ex_tableau.js
+++ ex_tableau.js
@@ -1,1 +1,19 @@
-var myT
+var myT=[]; 
+t=window.prompt("Ecrire le taille de tableau:"); 
+// t=myT.length;
+console.log(t);
+for (var i=1; i<=t; i++) 
+{
+  var b=window.prompt("Ecrire l'élément :");
+  myT[i]=b;
+  console.log(b);
+}
+
+for (var i=0; i<=myT.length; i++)
+{
+  console.log(myT[i]);
+}
+
+console.log("Fin de la boucle")
+
+
